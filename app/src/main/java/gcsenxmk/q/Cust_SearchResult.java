b070fe20169bdeea568852437f480966f5f169d0
Tey Siew Wen <estee.tsw@gmail.com>
2018-11-22 22:11:07 +0800

    SearchResult Recycler
    Changed QueueID to SearchResult.
    Made basic recycler view + card design layout for queues to be displayed in SearchResult.
    
    added implementation of glide and cardview

--- app/src/main/java/gcsenxmk/q/Cust_SearchResult.java
+++ app/src/main/java/gcsenxmk/q/Cust_SearchResult.java
@@ -3,11 +3,13 @@ package gcsenxmk.q;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Cust_QueueID extends AppCompatActivity {
+public class Cust_SearchResult extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_cust__queue_id);
+        setContentView(R.layout.activity_cust_search_result);
     }
+
+
 }
