7d1d37548818570cfe84a9158c20027cd323f373
Tey Siew Wen <35736525+lyqht@users.noreply.github.com>
2018-11-27 01:41:38 +0800

    Cust_Search_Result Recycler (#9)
    * Just widget references and button onClick Listeners
    
    * Made sample queue list item card layout
    
    * Made sample queue list item card layout
    
    * Made sample queue list item card layout and made recyclerview work.

--- app/src/main/java/gcsenxmk/q/Cust_SearchResult.java
+++ app/src/main/java/gcsenxmk/q/Cust_SearchResult.java
@@ -1,9 +1,11 @@
 package gcsenxmk.q;
 
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import java.util.ArrayList;
 
@@ -37,9 +39,20 @@ public class Cust_SearchResult extends AppCompatActivity {
     private void initializeData() {
         //TODO: Get the resources from XML File / SQLiteDatabase
 
-        String[] queueNames;
-        int[] queueWaitingTimes;
-        int[] queueNumPeople;
+        String[] queueNames = getResources().getStringArray(R.array.placeholder_queue_names);
+        int[] queueWaitingTimes = getResources().getIntArray(R.array.placeholder_queue_waiting_time);
+        int[] queueNumPeople = getResources().getIntArray(R.array.placeholder_queue_num_people);
+        TypedArray queueImages = getResources().obtainTypedArray(R.array.placeholder_queue_images);
+
+        int numQueues = queueNames.length;
+        for (int i = 0; i < numQueues; i ++) {
+            queues.add(new AlohaQueue(queueNames[i],
+                    queueWaitingTimes[i],
+                    queueNumPeople[i],
+                    queueImages.getResourceId(i, 0)));
+        }
+
+        Log.i("Logcat","able to initialize data");
 
         qAdapter.notifyDataSetChanged();
 
