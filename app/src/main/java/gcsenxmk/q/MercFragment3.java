2a21ca8de11a06e812d4b5cf7c710639a99c8611
Mcnath <mcnath45@gmail.com>
2018-12-03 21:46:08 +0800

    Fragment3 will mainpage for setting_merc

--- app/src/main/java/gcsenxmk/q/MercFragment3.java
+++ app/src/main/java/gcsenxmk/q/MercFragment3.java
@@ -7,23 +7,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MercFragment3 extends Fragment {
-    private static final String TAG = "Tab3Fragment";
-
-    private Button btnTEST;
+    private static final String TAG = "Tab3Fragment - Settings";
+    private Spinner queueTypeSpinner, lateSpinner, prioritySpinner;
+    private EditText editEstTimeText;
+    private Button saveSettingButton;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.merc_fragment3,container,false);
-        btnTEST = (Button) view.findViewById(R.id.btnTEST);
+        saveSettingButton= (Button) view.findViewById(R.id.saveSetting);
+        editEstTimeText = (EditText) view.findViewById(R.id.editEstTimeText);
 
-        btnTEST.setOnClickListener(new View.OnClickListener() {
+        saveSettingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 3",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "New Setting is saved",Toast.LENGTH_SHORT).show();
             }
         });
 
