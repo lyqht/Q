58125be8767fd868ea3d7260afb9d09a5844a403
SeanLim <32510237+LimSean@users.noreply.github.com>
2018-12-05 03:56:43 +0800

    Merchant (#17)
    * Queue Creation Activities added
    
    * frame for queue creation is out. To clean up the queue settings, and on click return to create queue page

---
+++ app/src/main/java/gcsenxmk/q/MercQueueCreated.java
@@ -0,0 +1,13 @@
+package gcsenxmk.q;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MercQueueCreated extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.merc_queue_created);
+    }
+}
