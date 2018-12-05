0502d3f5a5da18fe1e894870687f3607d0163917
SeanLim <32510237+LimSean@users.noreply.github.com>
2018-12-05 17:51:14 +0800

    Merchant (#22)
    * Queue Creation Activities added
    
    * frame for queue creation is out. To clean up the queue settings, and on click return to create queue page
    
    * manifest edited
    
    * mer create queueu layout is cleaned
    
    * navigation backstack clearing is added. On touch return to mermainactivity is added
    
    * queue created page widgets  and layout completed.
    
    * queue created image file to be pushed
    
    * help button resized.
    
    * removed late policy, further adjusted layout and respective java classes

--- app/src/main/java/gcsenxmk/q/MercCreateQueue.java
+++ app/src/main/java/gcsenxmk/q/MercCreateQueue.java
@@ -10,10 +10,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import android.widget.Toast;
+
+
 public class MercCreateQueue extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     EditText location;
-    private Spinner queueTypeSpinner, lateSpinner, prioritySpinner;
+private Spinner queueTypeSpinner, prioritySpinner;
+
     private EditText editEstTimeText;
     private Button btn_createQueue;
     EditText Qname;
@@ -35,17 +39,10 @@ public class MercCreateQueue extends AppCompatActivity implements AdapterView.On
         queueTypeSpinner.setAdapter(adapterQueue);
         queueTypeSpinner.setOnItemSelectedListener(this);
 
-        lateSpinner = findViewById(R.id.spinLate);
-        ArrayAdapter<CharSequence> adapterLate = ArrayAdapter.createFromResource(this,
-                R.array.late_policy, android.R.layout.simple_spinner_item);
-        adapterLate.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        lateSpinner.setAdapter(adapterLate);
-        lateSpinner.setOnItemSelectedListener(this);
-
         prioritySpinner = findViewById(R.id.spinPriority);
         ArrayAdapter<CharSequence> adapterPriority = ArrayAdapter.createFromResource(this,
                 R.array.priority_queue, android.R.layout.simple_spinner_item);
-        adapterLate.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
         prioritySpinner.setAdapter(adapterPriority);
         prioritySpinner.setOnItemSelectedListener(this);
 
@@ -53,7 +50,12 @@ public class MercCreateQueue extends AppCompatActivity implements AdapterView.On
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MercCreateQueue.this, MercQueueCreated.class);
+
+                //TODO link to firebase submission of data
+                //TODO condition to check if queue creation successful
+                Toast.makeText(MercCreateQueue.this, "Queue Created!", Toast.LENGTH_SHORT).show();
                 startActivity(intent);
+                finish();
             }
         });
     }
