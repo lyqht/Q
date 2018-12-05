0fa684179cf038ab12076ba7a69821390c453e61
SeanLim <kh2_rox@hotmail.com>
2018-12-05 14:41:46 +0800

    queue created page widgets  and layout completed.

--- app/src/main/java/gcsenxmk/q/MercQueueCreated.java
+++ app/src/main/java/gcsenxmk/q/MercQueueCreated.java
@@ -2,22 +2,25 @@ package gcsenxmk.q;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MercQueueCreated extends AppCompatActivity {
 
     TextView gobackmercmainactivity;
+    ImageView queuecreated;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.merc_queue_created);
-
+        queuecreated = findViewById(R.id.queuecreatedimage);
         gobackmercmainactivity = findViewById(R.id.gobackmercmainactivity);
 
         gobackmercmainactivity.setOnTouchListener(new View.OnTouchListener() {
