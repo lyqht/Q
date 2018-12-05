937d46cfa6ed409a7019dedce1f1337f27002b7c
gcgloven <gong.chen.gloven@gmail.com>
2018-12-05 17:43:18 +0800

    updated Grid
    Still has problem with collapsing

--- app/src/main/java/gcsenxmk/q/CustExplore.java
+++ app/src/main/java/gcsenxmk/q/CustExplore.java
@@ -1,5 +1,6 @@
 package gcsenxmk.q;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,7 +9,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.util.Log;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.SearchView;
 
 public class CustExplore extends Fragment {
 
@@ -19,7 +24,8 @@ public class CustExplore extends Fragment {
     ImageButton imageButtonLimitedT;
     ImageButton imageButtonExtra;
     ImageButton imageButtonRetail;
-
+    SearchView mSearch;
+    EditText mysearchView;
     private static final String TAG = "CustExplore";
 
     @Nullable
@@ -34,8 +40,26 @@ public class CustExplore extends Fragment {
         imageButtonRetail = view.findViewById(R.id.buttonRetail_Outlets);
         imageButtonLimitedT = view.findViewById(R.id.buttonLimitedT);
         imageButtonExtra = view.findViewById(R.id.buttonExtraCategory);
+        mSearch =view.findViewById(R.id.cme_editTxt_SearchBar);
+        mSearch.setIconified(false);
 
+        /*mysearchView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+               mysearchView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.showSoftInput(mysearchView, InputMethodManager.SHOW_IMPLICIT);
+                    }
+                });
+            }
+        });
+        mysearchView.requestFocus();*/
         return view;
     }
+
+
+
 
 }
