7a13c7b461ced0cc4349364cb4c8d3d300fed02b
Mcnath <mcnath45@gmail.com>
2018-12-07 19:42:34 +0800

    HelpPage
    Add 3 more activities for the HelpPage:
    Delete, config and create queue

---
+++ app/src/main/java/gcsenxmk/q/merchant/Merc_HelpPage_ConfigQueue.java
@@ -0,0 +1,14 @@
+package gcsenxmk.q.merchant;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Merc_HelpPage_ConfigQueue extends AppCompatActivity{
+    private static final String TAG = "Helppage_merc_configQueue";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        //setContentView(R.id.);
+    }
+}
---
+++ app/src/main/java/gcsenxmk/q/merchant/Merc_HelpPage_CreateQueue.java
@@ -0,0 +1,15 @@
+package gcsenxmk.q.merchant;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Merc_HelpPage_CreateQueue extends AppCompatActivity {
+    private static final String TAG = "Helppage_merc_createQueue";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        //setContentView(R.id.);
+    }
+
+}
