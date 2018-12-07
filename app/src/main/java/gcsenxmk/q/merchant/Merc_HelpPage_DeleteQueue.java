f54479016932e6241f6b1eca7f08a430ea2166b8
Mcnath <mcnath45@gmail.com>
2018-12-08 01:11:10 +0800

    Help Page
    create xml files for help page and complete the layout while fixing the issues.

--- app/src/main/java/gcsenxmk/q/merchant/Merc_HelpPage_DeleteQueue.java
+++ app/src/main/java/gcsenxmk/q/merchant/Merc_HelpPage_DeleteQueue.java
@@ -3,12 +3,14 @@ package gcsenxmk.q.merchant;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import gcsenxmk.q.R;
+
 public class Merc_HelpPage_DeleteQueue extends AppCompatActivity{
     private static final String TAG = "Helppage_merc";
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        //setContentView(R.id.);
+        setContentView(R.layout.merc_help_page_delete);
     }
 }
